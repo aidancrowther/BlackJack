ed3bb1bc0087fa2d3ecade88d5a6a1dc30be3782
aidancrowther <aidan.crowther@outlook.com>
2018-09-17 15:36:45 -0400

    Test that player correctly adds cards and their values, along with treating aces correctly

--- blackjack/src/test/java/aidancrowther/blackjack/PlayerTest.java
+++ blackjack/src/test/java/aidancrowther/blackjack/PlayerTest.java
@@ -35,13 +35,47 @@ public class PlayerTest
     }
 
     @Test
+    //Assert that dealer hand shows fully at end of game
+    public void CheckDealerEndHand(){
+        Boolean isDealer = true;
+        Boolean gameOver = true;
+        Player dealer = new Player(isDealer);
+
+        dealer.giveCard(new Card("H", "K"));
+        dealer.giveCard(new Card("H", "A"));
+
+        assertTrue(dealer.toString(gameOver).split(" ").length == 2);
+    }
+
+    @Test
     //Test that players hands increment size when adding cards
     public void CheckPlayerDealing(){
-        
+
         Player player = new Player(false);
         assertTrue(player.getHandSize() == 0);
 
         player.giveCard(new Card("H", "K"));
         assertTrue(player.getHandSize() == 1);
+    }
+
+    @Test
+    //Test that player hand value increases correctly when adding cards
+    public void CheckPlayerHandValue(){
+
+        Player player = new Player(false);
+
+        assertTrue(player.getHandValue() == 0);
+
+        player.giveCard(new Card("H", "2"));
+        assertTrue(player.getHandValue() == 2);
+
+        player.giveCard(new Card("H", "A"));
+        assertTrue(player.getHandValue() == 13);
+
+        player.giveCard(new Card("C", "A"));
+        assertTrue(player.getHandValue() == 14);
+
+        player.giveCard(new Card("H", "K"));
+        assertTrue(player.getHandValue() == 24);
     }
 }
